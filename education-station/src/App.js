679be60a98bc3bcd893af130de2b1ccd76f90930
vmehrotra50 <varun.mehrotra50@gmail.com>
2022-10-01 15:53:31 -0400

    Create three app

---
+++ education-station/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import { Canvas } from '@react-three/fiber'
+
+function MainScene() {
+  return <>
+    <mesh 
+  </>;
+}
+
+function App() {
+  return (
+    <Canvas shadowMap>
+      <MainScene />
+    </Canvas>
+  );
+}
+
+export default App;
