aecf9cde0972da0473647407c5748754720561d2
vmehrotra50 <varun.mehrotra50@gmail.com>
2022-10-02 09:51:55 -0400

    main

--- education-station/src/Canvas.js
+++ education-station/src/Canvas.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Canvas } from "react-three-fiber";
 import { Scene } from "./scene/Scene";
-import ReactSound from "react-sound";
+import AudioPlayer from 'react-h5-audio-player';
 import Sound from './vid_lecture.mp3'
 
 export default function Lecture(props) {
+    const [play, setPlay] = useState(false);
 
   useEffect(() => {
     props.setShowNavBar(false);
@@ -12,11 +13,18 @@ export default function Lecture(props) {
     return () => {
         props.setShowNavBar(true);
     }
+    setPlay(true);
   }, [])
   
   return (
     <div>
-        {/* <ReactSound url={Sound} playStatus={"PLAYING"} /> */}
+        <AudioPlayer
+            // style={{ width: "300px" }}
+            autoPlay={true}
+            // layout="horizontal"
+            src={Sound}
+            // other props here
+        />
         <Canvas shadowMap>
             <Scene />
         </Canvas>
