1a7ebc985016668919e102cab65afd47f90dfd93
Rohan Mehta <rmehta98@gatech.edu>
2022-10-02 02:04:57 -0400

    setup overall structure for updating loading

--- education-station/src/pages/Loading/Loading.js
+++ education-station/src/pages/Loading/Loading.js
@@ -4,22 +4,66 @@ import { faCircleNotch, faCircleCheck, faCirclePlay} from '@fortawesome/free-sol
 import React, { useState } from 'react'
 
 function Loading() {
-    const [loadingTextbook, setloadingTextbook] = useState(false)
+    const [loadingTranscript, setLoadingTranscript] = useState(false)
     const [generatingSlides, setgeneratingSlides] = useState(true)
     const [animatingLecture, setanimatingLecture] = useState(false)
+    
+
+    const server_url = "http://localhost:3001"
+
+    const Playback = {
+        "transcript": null,
+        "animation": null,
+        "slides": null,
+    }
+
+    // Loading Trascript
+    fetch(
+        server_url + "/transcript"
+    ).then((res) => {
+        console.log("success");
+        Playback["transcript"] = res
+        setLoadingTranscript(false)
+    }).catch((err) => {
+        console.log("errpr")
+        console.log(err)
+    })
+
+    fetch(
+        server_url + "/slides"
+    ).then((res) => {
+        console.log("success");
+        Playback["slides"] = res
+        setgeneratingSlides(false)
+    }).catch((err) => {
+        console.log("errpr")
+        console.log(err)
+    })
+
+    fetch(
+        server_url + "/animations"
+    ).then((res) => {
+        console.log("success");
+        Playback["animation"] = res
+        setanimatingLecture(false)
+    }).catch((err) => {
+        console.log("errpr")
+        console.log(err)
+    })
+
     return (
         <>
             <div>
                 <div className="flex-container">
                     <div className="loadingPane">
-                        <h5 className={loadingTextbook ? "loadingStates" : "loadingStates yellowText"}>
-                            <FontAwesomeIcon icon={loadingTextbook ? faCircleNotch : faCircleCheck}
-                                title="Loaded Textbook State"
-                                className={loadingTextbook ? "fa-spin" : 'yellowText'} /> Loaded Textbook
+                        <h5 className={loadingTranscript ? "loadingStates" : "loadingStates yellowText"}>
+                            <FontAwesomeIcon icon={loadingTranscript ? faCircleNotch : faCircleCheck}
+                                title="Loaded Transcript State"
+                                className={loadingTranscript ? "fa-spin" : 'yellowText'} /> Loaded Transcript
                         </h5>
                         <h5 className={generatingSlides ? "loadingStates" : "loadingStates yellowText"}><FontAwesomeIcon icon={generatingSlides ? faCircleNotch : faCircleCheck} title="Generated Slides State" className={generatingSlides ? "fa-spin" : 'yellowText'} /> Generated Slides</h5>
                         <h5 className={animatingLecture ? "loadingStates" : "loadingStates yellowText"}><FontAwesomeIcon icon={animatingLecture ? faCircleNotch : faCircleCheck} title="Animating Lecture" className={animatingLecture ? "fa-spin" : 'yellowText'} /> Animated Lecture </h5>
-                        <button className={ !loadingTextbook && !generatingSlides && !animatingLecture? "continueButton yellowText enabledButton" : "continueButton disabledText"} disabled={!loadingTextbook && !generatingSlides && !animatingLecture}>
+                        <button className={ !loadingTranscript && !generatingSlides && !animatingLecture? "continueButton yellowText enabledButton" : "continueButton disabledText"} disabled={!loadingTranscript && !generatingSlides && !animatingLecture}>
                             <FontAwesomeIcon className="icon" icon={faCirclePlay} />Watch Lecture
                         </button>
                     </div>
